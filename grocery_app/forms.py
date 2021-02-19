1ce74521516ec999b393027ab89f120d405b5ef6
jsm-txt <hose153@gmail.com>
2021-02-19 01:27:07 -0600

    finished routes and templets
    finished routes and templets

--- grocery_app/forms.py
+++ grocery_app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, SelectField, SubmitField
+from wtforms import StringField, DateField, SelectField, SubmitField, FloatField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired, Length, URL
+from grocery_app.models import ItemCategory, GroceryStore, GroceryItem
 
 class GroceryStoreForm(FlaskForm):
     """Form for adding/updating a GroceryStore."""
@@ -10,7 +11,10 @@ class GroceryStoreForm(FlaskForm):
     # - title - StringField
     # - address - StringField
     # - submit button
-    pass
+    
+    title = StringField('Title',validators=[DataRequired(), Length(min=3, max=50)])
+    address = StringField('Address',validators=[DataRequired(), Length(min=3, max=50)])
+    submit = SubmitField('SuBmIt')
 
 class GroceryItemForm(FlaskForm):
     """Form for adding/updating a GroceryItem."""
@@ -22,4 +26,9 @@ class GroceryItemForm(FlaskForm):
     # - photo_url - StringField (use a URL validator)
     # - store - QuerySelectField (specify the `query_factory` param)
     # - submit button
-    pass
+    name = StringField('Name',validators=[DataRequired(), Length(min=3, max=50)])
+    price = FloatField('Price')
+    category = SelectField('Category', choices=ItemCategory.choices())
+    photo_url = StringField('Photo_url',validators=[URL()])
+    store = QuerySelectField('Store', query_factory=lambda: GroceryStore.query, allow_blank=False)
+    submit = SubmitField('Cool')
